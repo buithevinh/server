bc2f64617a3adde11b053d29d6a883e16e37e01a
DESKTOP-USNRTNN\buith <buithevinh1993@gmail.com>
2022-05-26 14:44:12 +0700

    update

--- api/imagehash.js
+++ api/imagehash.js
@@ -5,7 +5,11 @@ const router = express.Router();
 const path =  require('path');
 const multer = require('multer');
 const upload = multer()
-
+const tf  = require('@tensorflow/tfjs-node');
+const Jimp = require("jimp");
+const { default: axios } = require('axios');
+const modelURL = 'https://teachablemachine.withgoogle.com/models/xKlYuxUch/' + 'model.json';
+const metadataURL = 'https://teachablemachine.withgoogle.com/models/xKlYuxUch/' + 'metadata.json';
 
 const createHash = (fBuffer) => {
   return new Promise(res => {
@@ -36,6 +40,53 @@ router.post('/upload-image', upload.single('file'),async (req, res) => {
   res.json({
     status: 200,
     hash: hash
+  });
+})
+
+router.post('/get-tagging',upload.single('file'), async (req, res) => {
+  const metadata = await axios.get(metadataURL)
+  const fBuffer = req.file.buffer;
+  const image = await Jimp.read(fBuffer);
+  image.cover(224, 224, Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+
+  const NUM_OF_CHANNELS = 3;
+  let arrays = new Float32Array(224 * 224 * NUM_OF_CHANNELS);
+  let i = 0;
+  image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+    const pixel = Jimp.intToRGBA(image.getPixelColor(x, y));
+    pixel.r = pixel.r / 127.0 - 1;
+    pixel.g = pixel.g / 127.0 - 1;
+    pixel.b = pixel.b / 127.0 - 1;
+    pixel.a = pixel.a / 127.0 - 1;
+    arrays[i * NUM_OF_CHANNELS + 0] = pixel.r;
+    arrays[i * NUM_OF_CHANNELS + 1] = pixel.g;
+    arrays[i * NUM_OF_CHANNELS + 2] = pixel.b;
+    i++;
+  });
+  const outShape = [224, 224, NUM_OF_CHANNELS];
+  let img_tensor = tf.tensor3d(arrays, outShape, 'float32');
+  img_tensor = img_tensor.expandDims(0);
+  const labels = metadata.data.labels;
+  const model = await tf.loadLayersModel(modelURL);
+  let predictions = await model.predict(img_tensor).dataSync();
+ 
+  const total =  predictions.reduce((t, item) => {
+    return t += item;
+  }, 0)
+  const classify = [];
+  for (let i = 0; i < predictions.length; i++) {
+    const label = labels[i];
+    const probability = Math.round(predictions[i] * 100 / total);
+    if(probability > 0) {
+      classify.push({
+        label: label,
+        score: probability
+      })
+    }
+  }
+  res.json({
+    status: 200,
+    classify: classify
   });
 })
 module.exports = router;
