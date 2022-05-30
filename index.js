636378c33334931857c8975541cb3353b8b81ba3
DESKTOP-USNRTNN\buith <buithevinh1993@gmail.com>
2022-05-30 20:48:52 +0700

    up date

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const {IO} = require('./socketio')
 
 const http = require('http');
 const server = http.createServer(app);
-const io = IO(server)
+
 app.use(express.json({ extended: false }));
 
 const port = process.env.port || 8000;
+
+const io = IO(server)
 app.all('/', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
--- socketio.js
+++ socketio.js
@@ -1,11 +1,20 @@
 const { Server } = require("socket.io");
+const redis = require('redis')
+const redisAdapter = require('socket.io-redis');
+const host = '127.0.0.1';
+const port = process.env.port || 8000;
+const pub = redis.createClient(port, host);
+const sub = redis.createClient(port, host, {detect_buffers: true});
+
 let _io;
 const IO = (server) => {
   _io = new Server(server, {
     cors: {
       origins: '*',
-      credentials: true
-    }
+      credentials: true,
+      methods: ["GET", "POST"],
+    },
+    adapter: redisAdapter({pubClient: pub, subClient: sub})
   });
   return _io 
 }
