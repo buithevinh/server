96df2435b6674f0301c5f9b582a81b3b639ffa84
DESKTOP-USNRTNN\buith <buithevinh1993@gmail.com>
2022-06-10 17:24:16 +0700

    add

--- sql/index.js
+++ sql/index.js
@@ -3,7 +3,7 @@ const queryTotalByScore = 'select COUNT(*) as `total` from `photos` where `categ
 const queryCategory = 'select *  from `photos` where `category` = ? LIMIT 100 OFFSET ?';
 const queryTotalCategory = 'select COUNT(*) as `total` from `photos` where `category` = ?';
 const queryInstagramPhotos =  'select *  from `photosInstagram` LIMIT 100 OFFSET ?';
-const sqlGetUserInstagrams = 'select *  from `informationInstagram` LIMIT 100';
+const sqlGetUserInstagrams = 'select *  from `informationInstagram` ORDER BY RAND() LIMIT 100';
 const sqlGetPhotoInstagrams = 'select *  from `photosInstagram` where `album_id` = ? LIMIT 100 OFFSET ?';
 const sqlGetUserByUserName = 'select *  from `informationInstagram` where `user_name` = ?'
 const sqlCountPhotoByUserName = 'select COUNT(*) as `total` from `photosInstagram` where `album_id` = ?';
