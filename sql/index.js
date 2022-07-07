f6f92826297af535b91c1ebac8a58c867d52e4d5
DESKTOP-USNRTNN\buith <buithevinh1993@gmail.com>
2022-07-07 23:39:17 +0700

    update sql

--- sql/index.js
+++ sql/index.js
@@ -3,9 +3,9 @@ const queryTotalByScore = 'select COUNT(*) as `total` from `photos` where `categ
 const queryCategory = 'select *  from `photos` where `category` = ? LIMIT 100 OFFSET ?';
 const queryTotalCategory = 'select COUNT(*) as `total` from `photos` where `category` = ?';
 const queryInstagramPhotos =  'select *  from `photosInstagram` where `album_id` in (?) LIMIT 100 OFFSET ?';
-const sqlGetUserInstagrams = 'select *  from `informationInstagram` ORDER BY RAND() LIMIT 100';
-const sqlGetPhotoInstagrams = 'select *  from `photosInstagram` where `album_id` = ? LIMIT 100 OFFSET ?';
-const sqlGetUserByUserName = 'select *  from `informationInstagram` where `user_name` = ?'
+const sqlGetUserInstagrams = 'select * from `informationInstagram` ORDER BY RAND() LIMIT 100';
+const sqlGetPhotoInstagrams = 'select *  from `photosInstagram` as ph INNER JOIN (select id from `photosInstagram` where `album_id` = ? LIMIT 100 OFFSET ?) as temp ON ph.id = temp.id';
+const sqlGetUserByUserName = 'select * from `informationInstagram` where `user_name` = ?'
 const sqlCountPhotoByUserName = 'select COUNT(*) as `total` from `photosInstagram` where `album_id` = ?';
 const sqlTotalInstagram = 'select COUNT(*) as `total` from `photosInstagram` where `album_id` in (?)';
 const sqlGetUserByUserNames = 'select *  from `informationInstagram` where user_name in (?)';
